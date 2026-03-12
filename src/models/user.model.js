7be5738433fd2bce3cde22fb044921760b9c8e87
Technoritesh152005 <khilariritesh61@gmail.com>
2026-03-12 13:07:12 +0530

    finished with controller and all routes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,167 +3,170 @@ import mongoose from 'mongoose'
 const userSchema = mongoose.Schema(
     {
 
-        name:{
-            type:String,
-            required:[true,"Please provide your name"],
-            trim:true,
-            maxLength:[30,"Size cannot exceed more than 30 characters"],
+        name: {
+            type: String,
+            required: [true, "Please provide your name"],
+            trim: true,
+            maxLength: [30, "Size cannot exceed more than 30 characters"],
         },
-        email:{
-            type:String,
-            required:[true,"Please provide your email"],
-            trim:true,
-            maxLength:[25,"Size cannot exceed more than 25 chaacters"],
-            match:[
+        email: {
+            type: String,
+            required: [true, "Please provide your email"],
+            trim: true,
+            maxLength: [25, "Size cannot exceed more than 25 chaacters"],
+            match: [
                 /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
                 'Please provide a valid email',
-              ],
-            lowercase:true,
-            unique:true,
+            ],
+            lowercase: true,
+            unique: true,
         },
-        password:{
-            type:String,
-            required:[true, "Please ensure to fill your Password"],
-            minLength:[8,"Size cannot be less than 8 Characters"],
+        password: {
+            type: String,
+            required: [true, "Please ensure to fill your Password"],
+            minLength: [8, "Size cannot be less than 8 Characters"],
             // anyone should not be able to sleect this
-            select:false,
+            select: false,
         },
-        role:{
-            type:String,
+        role: {
+            type: String,
             // enum means takes properties from ROLES
-            enum:Object.values(ROLES),
-            default:ROLES.USER,
+            enum: Object.values(ROLES),
+            default: ROLES.USER,
         },
-        isEmailVerified:{
-            type:String,
-            default:false,
+        isEmailVerified: {
+            type: String,
+            default: false,
         },
-        avatar:{
-            type:String,
+        avatar: {
+            type: String,
         },
-        phone:{
-            type:Number,
-            maxLength:[12,"Phone number must be 12 or less than 12 number"]
+        phone: {
+            type: Number,
+            maxLength: [12, "Phone number must be 12 or less than 12 number"]
         },
-        location:{
-            type:String
+        location: {
+            type: String
         },
-        bio:{
-            type:String,
-            maxLength:[300,"Should be less than 300 characters"]
+        bio: {
+            type: String,
+            maxLength: [300, "Should be less than 300 characters"]
         },
-        preference:{
-            hoursPerWeek:{
-                type:Number,
-                default:8,
-                min:1,
-                max:10
+        preference: {
+            hoursPerWeek: {
+                type: Number,
+                default: 8,
+                min: 1,
+                max: 10
+            }
+            ,
+            budget: {
+                type: String,
+                enum: ["free", "low", "medium", "high"],
+                default: "free",
+            },
+            learningStyle: {
+                type: String,
+                enum: ["mixed", "auditory", "visual", "reading", "kinesthetic"]
+            },
+            notification: {
+            email: {
+                type: Boolean,
+                default: true,
+            },
+            roadMapUpdates: {
+                type: Boolean,
+                default: true,
+            },
+            weeklyProgess: {
+                type: Boolean,
+                default: true
             }
         },
-        budget:{
-            type:String,
-            enum:["free","low","medium","high"],
-            default:"free",
         },
-        learningStyle:{
-            type:String,
-            enum:["mixed","auditory","visual","reading","kinesthetic"]
-        },
-        notification:{
-            email:{
-                type:Boolean,
-                default:true,
-            },
-            roadMapUpdates:{
-                type:Boolean,
-                default:true,
-            },
-            weeklyProgess:{
-                type:Boolean,
-                default:true
-            }
-        },
-        lastLogin:{
-            type:Date,
+
+        
+        lastLogin: {
+            type: Date,
         }
     },
     {
-        timestamps:true,
-        toJson:{
-            virtuals:true
+        timestamps: true,
+        toJson: {
+            virtuals: true
         },
-        toObject:{
-            virtuals:true
+        toObject: {
+            virtuals: true
         }
     }
 )
 
-userSchema.index({createdAt:-1})
+userSchema.index({ createdAt: -1 })
 // instead of suppose user can create multiple resume so if we want user partcular resume which is in other collection we use virtuals
 // virtuals takes collection name and searchacc tto user_id
 // helps not to call db everytime by db.findbyid(id).populate("resume")
 
 // resumes is name of virtual field
-userSchema.virtual("resumes",{
-    ref:"Resume",
+userSchema.virtual("resumes", {
+    ref: "Resume",
     // use the _id of user
-    localField:'_id',
+    localField: '_id',
     // in Resume collection look at user field and match local field
-    foreignField:'user'
+    foreignField: 'user'
 })
 
-userSchema.virtual('virtuals',{
-    ref:'Virtuals',
-    
+userSchema.virtual('virtuals', {
+    ref: 'Virtuals',
+
 })
 
 // pre is a middleware before svaing to db and 'save is a hook'
-userSchema.pre('save', async()=>{
+userSchema.pre('save', async () => {
     // if password is not modified means only other details are modifief like email , name.. so no need to calculate hash
-    if(!this.password.isModified()){
+    if (!this.password.isModified()) {
         next()
     }
     // 10 rounds of hashing  genSalt means generate random salt 10 times
     // here even if password has same password , hash will be different
     const salt = await bcrypt().genSalt(10)
     // to hash we require salt
-    this.password = await bcrypt.hash(this.password,salt)
+    this.password = await bcrypt.hash(this.password, salt)
 })
 
 // .methods means add a custom methods to document
-userSchema.methods.comparePassword = async function(eneteredPassword){
-    return await bcrypt.compare(this.password,eneteredPassword)
+userSchema.methods.comparePassword = async function (eneteredPassword) {
+    return await bcrypt.compare(this.password, eneteredPassword)
 }
 
 // now u need to create jwt access and refresh tokens
-userSchema.methods.generateAccessToken = async function(){
+userSchema.methods.generateAccessToken = async function () {
 
     return jwt.sign(
         {
-            _id:this.id,
-            email:this.email,
-            name:this.name,
-            role:this.role
+            _id: this.id,
+            email: this.email,
+            name: this.name,
+            role: this.role
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
-            expiresIn:ACCESS_TOKEN_EXPIRY
+            expiresIn: ACCESS_TOKEN_EXPIRY
         }
     )
 }
 
-userSchema.methods.generateRefreshToken = async function(){
+userSchema.methods.generateRefreshToken = async function () {
 
     return jwt.sign(
 
         {
-            _id:this.id
+            _id: this.id
         },
         process.env.REFRESH_TOKEN_SECRET,
         {
-            expiresIn:REFRESH_TOKEN_EXPIRY
+            expiresIn: REFRESH_TOKEN_EXPIRY
         }
     )
 }
 
- export const userModel = mongoose.model('userModel',userSchema)
+export const userModel = mongoose.model('userModel', userSchema)
--- src/routes/roadmapRoutes.js
+++ src/routes/roadmapRoutes.js
@@ -1,3 +1,47 @@
 import {protectAccess} from '../middleware/authMiddleware'
 import express from 'express'
 const router = express.Router()
+import protectAccess from '../middleware/authMiddleware'
+import {
+    createRoadmap,
+    getMyRoadmaps,
+    getRoadmapByAnalysis,
+    updateReference,
+    getRoadmapById,
+    getProgressOfUser,
+    markItemComplete
+}
+from '../controllers/roadmapController'
+// rouets in roadmap
+// 1.generate roadmap
+/*
+2.get all roadmap
+3.get roadmap by analysis id
+4.get roadmap by id
+5.get users progress on roadmap
+6.mark item complete
+7.update roadmap preference
+*/
+
+// 1. create roadmap for user
+router.post('/',protectAccess,createRoadmap)
+
+// 2. get all roadmap
+router.get('/',protectAccess,getMyRoadmaps)
+
+// 3.get roadmap by analysis Id
+router.get('/analysis/:analysisId',protectAccess,getRoadmapByAnalysis)
+
+// 4.get roadmap by id
+router.get('/:id',protectAccess,getRoadmapById)
+
+// 5. get users progress on roadmap
+router.get('/:id/progress',protectAccess,getProgressOfUser)
+
+// 6. mark item complete
+router.put('/:id/mark-item-complete',protectAccess,markItemComplete)
+
+// 7.update roadmap prefernce
+router.put('/:id/update-prefrenece',protectAccess,updateReference)
+
+module.exports = router
